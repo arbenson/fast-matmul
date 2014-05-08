b618dc773d9352bc9ba1c374ed341ca338a93cd8
Austin Benson <arbenson@gmail.com>
2014-05-08 08:21:49 -0700

    Automatically generated Strassen code.

---
+++ strassen.hpp
@@ -0,0 +1,157 @@
+#ifndef _FAST_HPP_
+#define _FAST_HPP_
+
+
+#include "linalg.hpp"
+#ifdef _CILK_
+# include <cilk/cilk.h>
+#endif
+
+
+template <typename Scalar>
+void FastMatmul(Matrix<Scalar>& A, Matrix<Scalar>& B, Matrix<Scalar>& C, int numsteps) {
+    // Base case for recursion
+    if (numsteps == 0) {
+        Gemm(A, B, C);
+        return;
+    }
+
+    int A_x_step = A.m() / 2;
+    int A_y_step = A.n() / 2;
+    Matrix<Scalar> A11(A.data() + 0 * A_x_step + 0 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    Matrix<Scalar> A12(A.data() + 0 * A_x_step + 1 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    Matrix<Scalar> A21(A.data() + 1 * A_x_step + 0 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    Matrix<Scalar> A22(A.data() + 1 * A_x_step + 1 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    int B_x_step = B.m() / 2;
+    int B_y_step = B.n() / 2;
+    Matrix<Scalar> B11(B.data() + 0 * B_x_step + 0 * B_y_step * B.stride(), B.stride(), B_x_step, B_y_step);
+    Matrix<Scalar> B12(B.data() + 0 * B_x_step + 1 * B_y_step * B.stride(), B.stride(), B_x_step, B_y_step);
+    Matrix<Scalar> B21(B.data() + 1 * B_x_step + 0 * B_y_step * B.stride(), B.stride(), B_x_step, B_y_step);
+    Matrix<Scalar> B22(B.data() + 1 * B_x_step + 1 * B_y_step * B.stride(), B.stride(), B_x_step, B_y_step);
+    int C_x_step = C.m() / 2;
+    int C_y_step = C.n() / 2;
+    Matrix<Scalar> C11(C.data() + 0 * C_x_step + 0 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+    Matrix<Scalar> C12(C.data() + 0 * C_x_step + 1 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+    Matrix<Scalar> C21(C.data() + 1 * C_x_step + 0 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+    Matrix<Scalar> C22(C.data() + 1 * C_x_step + 1 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+
+
+    // These are the intermediate matrices.
+    // We define them here so that they can be used
+    // inside the lambda functions for Cilk.
+    Matrix<Scalar> M1(C_x_step, C_y_step);
+    Matrix<Scalar> M2(C_x_step, C_y_step);
+    Matrix<Scalar> M3(C_x_step, C_y_step);
+    Matrix<Scalar> M4(C_x_step, C_y_step);
+    Matrix<Scalar> M5(C_x_step, C_y_step);
+    Matrix<Scalar> M6(C_x_step, C_y_step);
+    Matrix<Scalar> M7(C_x_step, C_y_step);
+
+
+    // M1 = (1.0 * A11 + 1.0 * A22) * (1.0 * B11 + 1.0 * B22)
+#ifdef _CILK_
+    cilk_spawn [&]{
+#endif
+    Matrix<Scalar> M1A(A11.m(), A11.n());
+    Add(A11, A22, Scalar(1), Scalar(1), M1A);
+    Matrix<Scalar> M1B(B11.m(), B11.n());
+    Add(B11, B22, Scalar(1), Scalar(1), M1B);
+    FastMatmul(M1A, M1B, M1, numsteps - 1);
+    M1A.deallocate();
+    M1B.deallocate();
+#ifdef _CILK_
+    }();
+#endif
+
+    // M2 = (1.0 * A21 + 1.0 * A22) * (1.0 * B11)
+#ifdef _CILK_
+    cilk_spawn [&]{
+#endif
+    Matrix<Scalar> M2A(A11.m(), A11.n());
+    Add(A21, A22, Scalar(1), Scalar(1), M2A);
+    
+    FastMatmul(M2A, B11, M2, numsteps - 1);
+    M2A.deallocate();
+#ifdef _CILK_
+    }();
+#endif
+
+    // M3 = (1.0 * A11) * (1.0 * B12 + -1.0 * B22)
+#ifdef _CILK_
+    cilk_spawn [&]{
+#endif
+    
+    Matrix<Scalar> M3B(B11.m(), B11.n());
+    Add(B12, B22, Scalar(1), Scalar(-1), M3B);
+    FastMatmul(A11, M3B, M3, numsteps - 1);
+    M3B.deallocate();
+#ifdef _CILK_
+    }();
+#endif
+
+    // M4 = (1.0 * A22) * (-1.0 * B11 + 1.0 * B21)
+#ifdef _CILK_
+    cilk_spawn [&]{
+#endif
+    
+    Matrix<Scalar> M4B(B11.m(), B11.n());
+    Add(B11, B21, Scalar(-1), Scalar(1), M4B);
+    FastMatmul(A22, M4B, M4, numsteps - 1);
+    M4B.deallocate();
+#ifdef _CILK_
+    }();
+#endif
+
+    // M5 = (1.0 * A11 + 1.0 * A12) * (1.0 * B22)
+#ifdef _CILK_
+    cilk_spawn [&]{
+#endif
+    Matrix<Scalar> M5A(A11.m(), A11.n());
+    Add(A11, A12, Scalar(1), Scalar(1), M5A);
+    
+    FastMatmul(M5A, B22, M5, numsteps - 1);
+    M5A.deallocate();
+#ifdef _CILK_
+    }();
+#endif
+
+    // M6 = (-1.0 * A11 + 1.0 * A21) * (1.0 * B11 + 1.0 * B12)
+#ifdef _CILK_
+    cilk_spawn [&]{
+#endif
+    Matrix<Scalar> M6A(A11.m(), A11.n());
+    Add(A11, A21, Scalar(-1), Scalar(1), M6A);
+    Matrix<Scalar> M6B(B11.m(), B11.n());
+    Add(B11, B12, Scalar(1), Scalar(1), M6B);
+    FastMatmul(M6A, M6B, M6, numsteps - 1);
+    M6A.deallocate();
+    M6B.deallocate();
+#ifdef _CILK_
+    }();
+#endif
+
+    // M7 = (1.0 * A12 + -1.0 * A22) * (1.0 * B21 + 1.0 * B22)
+#ifdef _CILK_
+    cilk_spawn [&]{
+#endif
+    Matrix<Scalar> M7A(A11.m(), A11.n());
+    Add(A12, A22, Scalar(1), Scalar(-1), M7A);
+    Matrix<Scalar> M7B(B11.m(), B11.n());
+    Add(B21, B22, Scalar(1), Scalar(1), M7B);
+    FastMatmul(M7A, M7B, M7, numsteps - 1);
+    M7A.deallocate();
+    M7B.deallocate();
+#ifdef _CILK_
+    }();
+#endif
+
+#ifdef _CILK_
+    cilk_sync;
+#endif
+    Add(M1, M4, M5, M7, Scalar(1), Scalar(1), Scalar(-1), Scalar(1), C11);
+    Add(M3, M5, Scalar(1), Scalar(1), C12);
+    Add(M2, M4, Scalar(1), Scalar(1), C21);
+    Add(M1, M2, M3, M6, Scalar(1), Scalar(-1), Scalar(1), Scalar(1), C22);
+}
+
+#endif  // _FAST_HPP_
