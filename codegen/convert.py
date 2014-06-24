1c7edb00caca788e5b0b9e3745091c57d4db9523
Austin Benson <arbenson@gmail.com>
2014-06-24 08:25:08 -0700

    Updated script for coefficient conversion.

--- codegen/convert.py
+++ codegen/convert.py
@@ -1,5 +1,4 @@
 import sys
-from util import *
 
 '''
 This script converts coefficients for <m, k, n> algorithms to all permutations:
@@ -19,11 +18,19 @@ where
 coeff_file is the name of the original coefficient file
 m,k,n is the original algorithm dimensions
 out_file is the name of the file in which to write the new coefficients
-s1,s2,s3 i the permutation, e.g. 1,2,0 for the <k, n, m> algorithm
+s1,s2,s3 is the permutation, e.g. 1,2,0 for the <k, n, m> algorithm
 '''
 
+
 def transpose(coeff_set, m, n):
-    ''' Return a transpose of the coefficient set. '''
+    ''' Return a transpose of the coefficient set.
+    coeff_set is the original set of coefficients
+    m is the number of rows
+    n is the number of columns
+    '''
+    if len(coeff_set) != m * n:
+        raise Exception('Incorrect number of entries in coefficient set')
+
     new_coeff_set = []
     for j in range(n):
         for i in range(m):
@@ -38,27 +45,55 @@ def replicate(coeff_set):
 
 
 def swap(coeffs, dims):
-    ''' Return the new coefficients for the swapped algorithm, i.e., if
+    ''' Get the new coefficients for the swapped algorithm, i.e., if
     coeffs comes from <m, k, n>, the return value contains the coefficients
-    for the <n, k, m> algorithm.
+    for the <n, k, m> algorithm.  The first return value is the new coefficient
+    set and the second return value contains the new algorithm dimensions.
+
+    coeffs are the original coefficiets
+    dims is a 3-tuple of the algorithm dimensions
     '''
     new_coeffs = []
+    new_coeffs.append(transpose(coeffs[1], dims[1], dims[2]))
     new_coeffs.append(transpose(coeffs[0], dims[0], dims[1]))
-    new_coeffs.append(replicate(coeffs[1]))
-    new_coeffs.append(transpose(coeffs[2], dims[1], dims[2]))
+    new_coeffs.append(transpose(coeffs[2], dims[0], dims[2]))
     return new_coeffs, (dims[2], dims[1], dims[0])
 
 
 def cyclic_right(coeffs, dims):
-    ''' Return the new coefficients for the cyclic right permutation, i.e., if
+    ''' Get the new coefficients for the cyclic right permutation, i.e., if
     coeffs comes from <m, k, n>, the return value contains the coefficients
-    for the <n, m, k> algorithm.
+    for the <n, m, k> algorithm.  The first return value is the new coefficient
+    set and the second return value contains the new algorithm dimensions.
+
+    coeffs are the original coefficiets
+    dims is a 3-tuple of the algorithm dimensions
     '''
     new_coeffs = []
     new_coeffs.append(transpose(coeffs[2], dims[0], dims[2]))
     new_coeffs.append(replicate(coeffs[0]))
     new_coeffs.append(transpose(coeffs[1], dims[1], dims[2]))
     return new_coeffs, (dims[2], dims[0], dims[1])
+
+
+def read_coeffs(filename):
+    ''' Read the coefficient file.  There is one group of coefficients for each
+    of the three matrices.  Keep the textual structure of the file. '''
+    coeffs = []
+    with open(filename, 'r') as coeff_file:
+        curr_group = []
+        for line in coeff_file:
+            if line[0] == '#':
+                if len(curr_group) > 0:
+                    coeffs.append(curr_group)
+                    curr_group = []
+            else:
+                curr_group.append([val for val in line.split()])
+    coeffs.append(curr_group)
+    # There should be three sets of coefficients: one for each matrix.
+    if (len(coeffs) != 3):
+        raise Exception('Expected three sets of coefficients!')
+    return coeffs
 
 
 def main():
@@ -70,10 +105,10 @@ def main():
         print 'Reading coefficients for %d x %d x %d matrix' % dims
         print 'Converting to %d x %d x %d matrix' % (dims[perm[0]], dims[perm[1]], dims[perm[2]])
     except:
-        raise Exception('USAGE: python convert.py coeff_file m,n,p out_file s1,s2,s3')
+        raise Exception('USAGE: python convert.py coeff_file m,k,n out_file s1,s2,s3')
 
     coeffs = read_coeffs(sys.argv[1])
-    new_coeffs = []
+
     if perm == (0, 1, 2):
         new_coeffs = [replicate(coeffs[i]) for i in range(3)]
     elif perm == (2, 0, 1):
@@ -90,14 +125,15 @@ def main():
         new_coeffs, _ = swap(new_coeffs, new_dims)
     elif perm == (2, 1, 0):
         new_coeffs, _ = swap(coeffs, dims)
-
-
+    else:
+        raise Exception('Invalid permutation %d x %d %d' % perm)
 
     with open(sys.argv[3], 'w') as out_file:
         out_file.write('# (%d, %d, %d) permutation of %s\n' % (perm[0], perm[1], perm[2], coeff_file))
         def write_coeff_set(coeff_set):
-            print coeff_set
+            ''' Write the coefficient set for a single matrix (A, B, or C). '''
             for entry in coeff_set:
+                # Single (i, j) entry of a single matrix.
                 out_file.write(' '.join([str(val) for val in entry]) + '\n')
      
         write_coeff_set(new_coeffs[0])
@@ -105,7 +141,6 @@ def main():
         write_coeff_set(new_coeffs[1])
         out_file.write('#\n')
         write_coeff_set(new_coeffs[2])
-
 
 if __name__ == '__main__':
     main()
