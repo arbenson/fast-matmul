24fef2d45ad57cfa4ee06cc102e05d6677444c88
Austin Benson <arbenson@gmail.com>
2014-06-25 09:33:29 -0700

    New algorithms

---
+++ grey-fast422.hpp
@@ -0,0 +1,351 @@
+#ifndef _grey422_14_84_HPP_
+#define _grey422_14_84_HPP_
+
+
+// This is an automatically generated file from gen.py.
+#include "common.hpp"
+
+
+namespace grey422_14_84 {
+template <typename Scalar>
+void FastMatmul(Matrix<Scalar>& A, Matrix<Scalar>& B, Matrix<Scalar>& C, int numsteps, double x=1e-8) {
+    // Base case for recursion
+    if (numsteps == 0) {
+        Gemm(A, B, C);
+        return;
+    }
+
+    int A_x_step = A.m() / 4;
+    int A_y_step = A.n() / 2;
+    Matrix<Scalar> A11(A.data() + 0 * A_x_step + 0 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    Matrix<Scalar> A12(A.data() + 0 * A_x_step + 1 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    Matrix<Scalar> A21(A.data() + 1 * A_x_step + 0 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    Matrix<Scalar> A22(A.data() + 1 * A_x_step + 1 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    Matrix<Scalar> A31(A.data() + 2 * A_x_step + 0 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    Matrix<Scalar> A32(A.data() + 2 * A_x_step + 1 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    Matrix<Scalar> A41(A.data() + 3 * A_x_step + 0 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    Matrix<Scalar> A42(A.data() + 3 * A_x_step + 1 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    int B_x_step = B.m() / 2;
+    int B_y_step = B.n() / 2;
+    Matrix<Scalar> B11(B.data() + 0 * B_x_step + 0 * B_y_step * B.stride(), B.stride(), B_x_step, B_y_step);
+    Matrix<Scalar> B12(B.data() + 0 * B_x_step + 1 * B_y_step * B.stride(), B.stride(), B_x_step, B_y_step);
+    Matrix<Scalar> B21(B.data() + 1 * B_x_step + 0 * B_y_step * B.stride(), B.stride(), B_x_step, B_y_step);
+    Matrix<Scalar> B22(B.data() + 1 * B_x_step + 1 * B_y_step * B.stride(), B.stride(), B_x_step, B_y_step);
+    int C_x_step = C.m() / 4;
+    int C_y_step = C.n() / 2;
+    Matrix<Scalar> C11(C.data() + 0 * C_x_step + 0 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+    Matrix<Scalar> C12(C.data() + 0 * C_x_step + 1 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+    Matrix<Scalar> C21(C.data() + 1 * C_x_step + 0 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+    Matrix<Scalar> C22(C.data() + 1 * C_x_step + 1 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+    Matrix<Scalar> C31(C.data() + 2 * C_x_step + 0 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+    Matrix<Scalar> C32(C.data() + 2 * C_x_step + 1 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+    Matrix<Scalar> C41(C.data() + 3 * C_x_step + 0 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+    Matrix<Scalar> C42(C.data() + 3 * C_x_step + 1 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+
+
+    // These are the intermediate matrices.
+    // We define them here so that they can be used
+    // inside the lambda functions for Cilk.
+    Matrix<Scalar> M1(C_x_step, C_y_step);
+    Matrix<Scalar> M2(C_x_step, C_y_step);
+    Matrix<Scalar> M3(C_x_step, C_y_step);
+    Matrix<Scalar> M4(C_x_step, C_y_step);
+    Matrix<Scalar> M5(C_x_step, C_y_step);
+    Matrix<Scalar> M6(C_x_step, C_y_step);
+    Matrix<Scalar> M7(C_x_step, C_y_step);
+    Matrix<Scalar> M8(C_x_step, C_y_step);
+    Matrix<Scalar> M9(C_x_step, C_y_step);
+    Matrix<Scalar> M10(C_x_step, C_y_step);
+    Matrix<Scalar> M11(C_x_step, C_y_step);
+    Matrix<Scalar> M12(C_x_step, C_y_step);
+    Matrix<Scalar> M13(C_x_step, C_y_step);
+    Matrix<Scalar> M14(C_x_step, C_y_step);
+
+
+#ifdef _OPEN_MP_
+    #pragma omp parallel
+    {
+    #pragma omp single
+        {
+#endif
+    // M1 = (1.0 * A21 + -1.0 * A22 + -1.0 * A31 + 1.0 * A32) * (1.0 * B12)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M1A(A11.m(), A11.n());
+    Add(A21, A22, A31, A32, Scalar(1.0), Scalar(-1.0), Scalar(-1.0), Scalar(1.0), M1A);
+    FastMatmul(M1A, B12, M1, numsteps - 1, x);
+    M1A.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M2 = (-1.0 * A12 + 1.0 * A22 + -1.0 * A32) * (-1.0 * B21 + 1.0 * B22)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M2A(A11.m(), A11.n());
+    Add(A12, A22, A32, Scalar(-1.0), Scalar(1.0), Scalar(-1.0), M2A);
+    Matrix<Scalar> M2B(B11.m(), B11.n());
+    Add(B21, B22, Scalar(-1.0), Scalar(1.0), M2B);
+    FastMatmul(M2A, M2B, M2, numsteps - 1, x);
+    M2A.deallocate();
+    M2B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M3 = (-1.0 * A11 + 1.0 * A21 + -1.0 * A31) * (-1.0 * B11 + 1.0 * B12)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M3A(A11.m(), A11.n());
+    Add(A11, A21, A31, Scalar(-1.0), Scalar(1.0), Scalar(-1.0), M3A);
+    Matrix<Scalar> M3B(B11.m(), B11.n());
+    Add(B11, B12, Scalar(-1.0), Scalar(1.0), M3B);
+    FastMatmul(M3A, M3B, M3, numsteps - 1, x);
+    M3A.deallocate();
+    M3B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M4 = (1.0 * A41) * (1.0 * B11 + 1.0 * B21)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M4B(B11.m(), B11.n());
+    Add(B11, B21, Scalar(1.0), Scalar(1.0), M4B);
+    FastMatmul(A41, M4B, M4, numsteps - 1, x);
+    M4B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M5 = (1.0 * A32 + 1.0 * A41) * (1.0 * B12 + 1.0 * B21)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M5A(A11.m(), A11.n());
+    Add(A32, A41, Scalar(1.0), Scalar(1.0), M5A);
+    Matrix<Scalar> M5B(B11.m(), B11.n());
+    Add(B12, B21, Scalar(1.0), Scalar(1.0), M5B);
+    FastMatmul(M5A, M5B, M5, numsteps - 1, x);
+    M5A.deallocate();
+    M5B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M6 = (1.0 * A32 + 1.0 * A42) * (-1.0 * B21 + 1.0 * B22)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M6A(A11.m(), A11.n());
+    Add(A32, A42, Scalar(1.0), Scalar(1.0), M6A);
+    Matrix<Scalar> M6B(B11.m(), B11.n());
+    Add(B21, B22, Scalar(-1.0), Scalar(1.0), M6B);
+    FastMatmul(M6A, M6B, M6, numsteps - 1, x);
+    M6A.deallocate();
+    M6B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M7 = (1.0 * A11) * (1.0 * B11 + 1.0 * B21)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M7B(B11.m(), B11.n());
+    Add(B11, B21, Scalar(1.0), Scalar(1.0), M7B);
+    FastMatmul(A11, M7B, M7, numsteps - 1, x);
+    M7B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M8 = (-1.0 * A11 + 1.0 * A21 + 1.0 * A41) * (1.0 * B11 + -1.0 * B12)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M8A(A11.m(), A11.n());
+    Add(A11, A21, A41, Scalar(-1.0), Scalar(1.0), Scalar(1.0), M8A);
+    Matrix<Scalar> M8B(B11.m(), B11.n());
+    Add(B11, B12, Scalar(1.0), Scalar(-1.0), M8B);
+    FastMatmul(M8A, M8B, M8, numsteps - 1, x);
+    M8A.deallocate();
+    M8B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M9 = (1.0 * A11 + -1.0 * A22 + 1.0 * A32) * (1.0 * B12 + 1.0 * B21)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M9A(A11.m(), A11.n());
+    Add(A11, A22, A32, Scalar(1.0), Scalar(-1.0), Scalar(1.0), M9A);
+    Matrix<Scalar> M9B(B11.m(), B11.n());
+    Add(B12, B21, Scalar(1.0), Scalar(1.0), M9B);
+    FastMatmul(M9A, M9B, M9, numsteps - 1, x);
+    M9A.deallocate();
+    M9B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M10 = (-1.0 * A11 + 1.0 * A12) * (-1.0 * B21)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M10A(A11.m(), A11.n());
+    Add(A11, A12, Scalar(-1.0), Scalar(1.0), M10A);
+    Matrix<Scalar> M10B(B11.m(), B11.n());
+    Add(B21, Scalar(-1.0), M10B);
+    FastMatmul(M10A, M10B, M10, numsteps - 1, x);
+    M10A.deallocate();
+    M10B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M11 = (1.0 * A31 + -1.0 * A32) * (1.0 * B12)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M11A(A11.m(), A11.n());
+    Add(A31, A32, Scalar(1.0), Scalar(-1.0), M11A);
+    FastMatmul(M11A, B12, M11, numsteps - 1, x);
+    M11A.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M12 = (-1.0 * A41 + 1.0 * A42) * (1.0 * B21)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M12A(A11.m(), A11.n());
+    Add(A41, A42, Scalar(-1.0), Scalar(1.0), M12A);
+    FastMatmul(M12A, B21, M12, numsteps - 1, x);
+    M12A.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M13 = (-1.0 * A32) * (1.0 * B12 + 1.0 * B22)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M13A(A11.m(), A11.n());
+    Add(A32, Scalar(-1.0), M13A);
+    Matrix<Scalar> M13B(B11.m(), B11.n());
+    Add(B12, B22, Scalar(1.0), Scalar(1.0), M13B);
+    FastMatmul(M13A, M13B, M13, numsteps - 1, x);
+    M13A.deallocate();
+    M13B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M14 = (1.0 * A22) * (1.0 * B12 + 1.0 * B22)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M14B(B11.m(), B11.n());
+    Add(B12, B22, Scalar(1.0), Scalar(1.0), M14B);
+    FastMatmul(A22, M14B, M14, numsteps - 1, x);
+    M14B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+#ifdef _CILK_
+    cilk_sync;
+#elif defined _OPEN_MP_
+        }  // End omp single region
+    }  // End omp parallel region
+#endif
+    Add(M7, M10, Scalar(1.0), Scalar(-1.0), C11);
+    Add(M2, M9, M10, M13, M14, Scalar(-1.0), Scalar(1.0), Scalar(-1.0), Scalar(1.0), Scalar(1.0), C12);
+    Add(M1, M4, M5, M7, M8, M9, M11, Scalar(1.0), Scalar(-1.0), Scalar(1.0), Scalar(1.0), Scalar(1.0), Scalar(-1.0), Scalar(1.0), C21);
+    Add(M1, M11, M14, Scalar(1.0), Scalar(1.0), Scalar(1.0), C22);
+    Add(M3, M4, M5, M8, M11, Scalar(1.0), Scalar(-1.0), Scalar(1.0), Scalar(1.0), Scalar(1.0), C31);
+    Add(M11, M13, Scalar(1.0), Scalar(-1.0), C32);
+    Add(M4, M12, Scalar(1.0), Scalar(1.0), C41);
+    Add(M5, M6, M12, M13, Scalar(1.0), Scalar(1.0), Scalar(1.0), Scalar(1.0), C42);
+}
+
+}
+
+#endif  // _grey422_14_84_HPP_
---
+++ grey-fast522.hpp
@@ -0,0 +1,423 @@
+#ifndef _grey522_18_99_HPP_
+#define _grey522_18_99_HPP_
+
+
+// This is an automatically generated file from gen.py.
+#include "common.hpp"
+
+
+namespace grey522_18_99 {
+template <typename Scalar>
+void FastMatmul(Matrix<Scalar>& A, Matrix<Scalar>& B, Matrix<Scalar>& C, int numsteps, double x=1e-8) {
+    // Base case for recursion
+    if (numsteps == 0) {
+        Gemm(A, B, C);
+        return;
+    }
+
+    int A_x_step = A.m() / 5;
+    int A_y_step = A.n() / 2;
+    Matrix<Scalar> A11(A.data() + 0 * A_x_step + 0 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    Matrix<Scalar> A12(A.data() + 0 * A_x_step + 1 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    Matrix<Scalar> A21(A.data() + 1 * A_x_step + 0 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    Matrix<Scalar> A22(A.data() + 1 * A_x_step + 1 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    Matrix<Scalar> A31(A.data() + 2 * A_x_step + 0 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    Matrix<Scalar> A32(A.data() + 2 * A_x_step + 1 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    Matrix<Scalar> A41(A.data() + 3 * A_x_step + 0 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    Matrix<Scalar> A42(A.data() + 3 * A_x_step + 1 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    Matrix<Scalar> A51(A.data() + 4 * A_x_step + 0 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    Matrix<Scalar> A52(A.data() + 4 * A_x_step + 1 * A_y_step * A.stride(), A.stride(), A_x_step, A_y_step);
+    int B_x_step = B.m() / 2;
+    int B_y_step = B.n() / 2;
+    Matrix<Scalar> B11(B.data() + 0 * B_x_step + 0 * B_y_step * B.stride(), B.stride(), B_x_step, B_y_step);
+    Matrix<Scalar> B12(B.data() + 0 * B_x_step + 1 * B_y_step * B.stride(), B.stride(), B_x_step, B_y_step);
+    Matrix<Scalar> B21(B.data() + 1 * B_x_step + 0 * B_y_step * B.stride(), B.stride(), B_x_step, B_y_step);
+    Matrix<Scalar> B22(B.data() + 1 * B_x_step + 1 * B_y_step * B.stride(), B.stride(), B_x_step, B_y_step);
+    int C_x_step = C.m() / 5;
+    int C_y_step = C.n() / 2;
+    Matrix<Scalar> C11(C.data() + 0 * C_x_step + 0 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+    Matrix<Scalar> C12(C.data() + 0 * C_x_step + 1 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+    Matrix<Scalar> C21(C.data() + 1 * C_x_step + 0 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+    Matrix<Scalar> C22(C.data() + 1 * C_x_step + 1 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+    Matrix<Scalar> C31(C.data() + 2 * C_x_step + 0 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+    Matrix<Scalar> C32(C.data() + 2 * C_x_step + 1 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+    Matrix<Scalar> C41(C.data() + 3 * C_x_step + 0 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+    Matrix<Scalar> C42(C.data() + 3 * C_x_step + 1 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+    Matrix<Scalar> C51(C.data() + 4 * C_x_step + 0 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+    Matrix<Scalar> C52(C.data() + 4 * C_x_step + 1 * C_y_step * C.stride(), C.stride(), C_x_step, C_y_step);
+
+
+    // These are the intermediate matrices.
+    // We define them here so that they can be used
+    // inside the lambda functions for Cilk.
+    Matrix<Scalar> M1(C_x_step, C_y_step);
+    Matrix<Scalar> M2(C_x_step, C_y_step);
+    Matrix<Scalar> M3(C_x_step, C_y_step);
+    Matrix<Scalar> M4(C_x_step, C_y_step);
+    Matrix<Scalar> M5(C_x_step, C_y_step);
+    Matrix<Scalar> M6(C_x_step, C_y_step);
+    Matrix<Scalar> M7(C_x_step, C_y_step);
+    Matrix<Scalar> M8(C_x_step, C_y_step);
+    Matrix<Scalar> M9(C_x_step, C_y_step);
+    Matrix<Scalar> M10(C_x_step, C_y_step);
+    Matrix<Scalar> M11(C_x_step, C_y_step);
+    Matrix<Scalar> M12(C_x_step, C_y_step);
+    Matrix<Scalar> M13(C_x_step, C_y_step);
+    Matrix<Scalar> M14(C_x_step, C_y_step);
+    Matrix<Scalar> M15(C_x_step, C_y_step);
+    Matrix<Scalar> M16(C_x_step, C_y_step);
+    Matrix<Scalar> M17(C_x_step, C_y_step);
+    Matrix<Scalar> M18(C_x_step, C_y_step);
+
+
+#ifdef _OPEN_MP_
+    #pragma omp parallel
+    {
+    #pragma omp single
+        {
+#endif
+    // M1 = (-1.0 * A32) * (1.0 * B21)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M1A(A11.m(), A11.n());
+    Add(A32, Scalar(-1.0), M1A);
+    FastMatmul(M1A, B21, M1, numsteps - 1, x);
+    M1A.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M2 = (1.0 * A41 + 1.0 * A52) * (1.0 * B11 + 1.0 * B22)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M2A(A11.m(), A11.n());
+    Add(A41, A52, Scalar(1.0), Scalar(1.0), M2A);
+    Matrix<Scalar> M2B(B11.m(), B11.n());
+    Add(B11, B22, Scalar(1.0), Scalar(1.0), M2B);
+    FastMatmul(M2A, M2B, M2, numsteps - 1, x);
+    M2A.deallocate();
+    M2B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M3 = (-1.0 * A31) * (1.0 * B11)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M3A(A11.m(), A11.n());
+    Add(A31, Scalar(-1.0), M3A);
+    FastMatmul(M3A, B11, M3, numsteps - 1, x);
+    M3A.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M4 = (1.0 * A52) * (1.0 * B11 + 1.0 * B21)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M4B(B11.m(), B11.n());
+    Add(B11, B21, Scalar(1.0), Scalar(1.0), M4B);
+    FastMatmul(A52, M4B, M4, numsteps - 1, x);
+    M4B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M5 = (1.0 * A11 + -1.0 * A12 + -1.0 * A21 + 1.0 * A22) * (-1.0 * B11 + 1.0 * B12)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M5A(A11.m(), A11.n());
+    Add(A11, A12, A21, A22, Scalar(1.0), Scalar(-1.0), Scalar(-1.0), Scalar(1.0), M5A);
+    Matrix<Scalar> M5B(B11.m(), B11.n());
+    Add(B11, B12, Scalar(-1.0), Scalar(1.0), M5B);
+    FastMatmul(M5A, M5B, M5, numsteps - 1, x);
+    M5A.deallocate();
+    M5B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M6 = (1.0 * A41) * (1.0 * B12 + 1.0 * B22)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M6B(B11.m(), B11.n());
+    Add(B12, B22, Scalar(1.0), Scalar(1.0), M6B);
+    FastMatmul(A41, M6B, M6, numsteps - 1, x);
+    M6B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M7 = (-1.0 * A41 + -1.0 * A51) * (1.0 * B11 + -1.0 * B12)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M7A(A11.m(), A11.n());
+    Add(A41, A51, Scalar(-1.0), Scalar(-1.0), M7A);
+    Matrix<Scalar> M7B(B11.m(), B11.n());
+    Add(B11, B12, Scalar(1.0), Scalar(-1.0), M7B);
+    FastMatmul(M7A, M7B, M7, numsteps - 1, x);
+    M7A.deallocate();
+    M7B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M8 = (1.0 * A31) * (1.0 * B12)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    FastMatmul(A31, B12, M8, numsteps - 1, x);
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M9 = (1.0 * A12 + -1.0 * A22) * (-1.0 * B11 + 1.0 * B12 + -1.0 * B21 + 1.0 * B22)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M9A(A11.m(), A11.n());
+    Add(A12, A22, Scalar(1.0), Scalar(-1.0), M9A);
+    Matrix<Scalar> M9B(B11.m(), B11.n());
+    Add(B11, B12, B21, B22, Scalar(-1.0), Scalar(1.0), Scalar(-1.0), Scalar(1.0), M9B);
+    FastMatmul(M9A, M9B, M9, numsteps - 1, x);
+    M9A.deallocate();
+    M9B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M10 = (1.0 * A42 + 1.0 * A52) * (-1.0 * B21 + 1.0 * B22)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M10A(A11.m(), A11.n());
+    Add(A42, A52, Scalar(1.0), Scalar(1.0), M10A);
+    Matrix<Scalar> M10B(B11.m(), B11.n());
+    Add(B21, B22, Scalar(-1.0), Scalar(1.0), M10B);
+    FastMatmul(M10A, M10B, M10, numsteps - 1, x);
+    M10A.deallocate();
+    M10B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M11 = (1.0 * A12 + 1.0 * A32) * (1.0 * B21)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M11A(A11.m(), A11.n());
+    Add(A12, A32, Scalar(1.0), Scalar(1.0), M11A);
+    FastMatmul(M11A, B21, M11, numsteps - 1, x);
+    M11A.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M12 = (-1.0 * A21 + 1.0 * A22 + -1.0 * A31) * (1.0 * B22)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M12A(A11.m(), A11.n());
+    Add(A21, A22, A31, Scalar(-1.0), Scalar(1.0), Scalar(-1.0), M12A);
+    FastMatmul(M12A, B22, M12, numsteps - 1, x);
+    M12A.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M13 = (-1.0 * A41 + 1.0 * A42) * (1.0 * B22)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M13A(A11.m(), A11.n());
+    Add(A41, A42, Scalar(-1.0), Scalar(1.0), M13A);
+    FastMatmul(M13A, B22, M13, numsteps - 1, x);
+    M13A.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M14 = (-1.0 * A12 + -1.0 * A21 + 1.0 * A22 + -1.0 * A31) * (-1.0 * B11 + 1.0 * B12 + 1.0 * B22)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M14A(A11.m(), A11.n());
+    Add(A12, A21, A22, A31, Scalar(-1.0), Scalar(-1.0), Scalar(1.0), Scalar(-1.0), M14A);
+    Matrix<Scalar> M14B(B11.m(), B11.n());
+    Add(B11, B12, B22, Scalar(-1.0), Scalar(1.0), Scalar(1.0), M14B);
+    FastMatmul(M14A, M14B, M14, numsteps - 1, x);
+    M14A.deallocate();
+    M14B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M15 = (1.0 * A11 + 1.0 * A31) * (1.0 * B11)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M15A(A11.m(), A11.n());
+    Add(A11, A31, Scalar(1.0), Scalar(1.0), M15A);
+    FastMatmul(M15A, B11, M15, numsteps - 1, x);
+    M15A.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M16 = (1.0 * A21 + 1.0 * A31) * (1.0 * B12 + 1.0 * B22)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M16A(A11.m(), A11.n());
+    Add(A21, A31, Scalar(1.0), Scalar(1.0), M16A);
+    Matrix<Scalar> M16B(B11.m(), B11.n());
+    Add(B12, B22, Scalar(1.0), Scalar(1.0), M16B);
+    FastMatmul(M16A, M16B, M16, numsteps - 1, x);
+    M16A.deallocate();
+    M16B.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M17 = (-1.0 * A32) * (1.0 * B22)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M17A(A11.m(), A11.n());
+    Add(A32, Scalar(-1.0), M17A);
+    FastMatmul(M17A, B22, M17, numsteps - 1, x);
+    M17A.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+    // M18 = (1.0 * A51 + -1.0 * A52) * (1.0 * B11)
+#ifdef _CILK_
+    cilk_spawn [&] {
+#elif defined _OPEN_MP_
+# pragma omp task
+    {
+#endif
+    Matrix<Scalar> M18A(A11.m(), A11.n());
+    Add(A51, A52, Scalar(1.0), Scalar(-1.0), M18A);
+    FastMatmul(M18A, B11, M18, numsteps - 1, x);
+    M18A.deallocate();
+#ifdef _CILK_
+    }();
+#elif defined _OPEN_MP_
+    }
+#endif
+
+#ifdef _CILK_
+    cilk_sync;
+#elif defined _OPEN_MP_
+        }  // End omp single region
+    }  // End omp parallel region
+#endif
+    Add(M1, M3, M11, M15, Scalar(1.0), Scalar(1.0), Scalar(1.0), Scalar(1.0), C11);
+    Add(M5, M8, M12, M14, M15, Scalar(1.0), Scalar(-1.0), Scalar(1.0), Scalar(-1.0), Scalar(1.0), C12);
+    Add(M1, M3, M9, M11, M14, M16, Scalar(1.0), Scalar(1.0), Scalar(1.0), Scalar(1.0), Scalar(1.0), Scalar(1.0), C21);
+    Add(M8, M12, M16, Scalar(-1.0), Scalar(1.0), Scalar(1.0), C22);
+    Add(M1, M3, Scalar(-1.0), Scalar(-1.0), C31);
+    Add(M8, M17, Scalar(1.0), Scalar(-1.0), C32);
+    Add(M2, M4, M10, M13, Scalar(1.0), Scalar(-1.0), Scalar(-1.0), Scalar(1.0), C41);
+    Add(M6, M13, Scalar(1.0), Scalar(1.0), C42);
+    Add(M4, M18, Scalar(1.0), Scalar(1.0), C51);
+    Add(M2, M6, M7, M18, Scalar(1.0), Scalar(-1.0), Scalar(1.0), Scalar(1.0), C52);
+}
+
+}
+
+#endif  // _grey522_18_99_HPP_
