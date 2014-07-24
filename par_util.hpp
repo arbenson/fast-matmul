c7c3d6ec07464ba72cd61b11177be2e284683601
Austin Benson <arbenson@gmail.com>
2014-07-23 17:02:32 -0700

    Get calls on launching tasks / waiting working properly.

--- par_util.hpp
+++ par_util.hpp
@@ -1,63 +1,100 @@
 #ifndef _PAR_UTIL_
 #define _PAR_UTIL_
 
+#include "math.h"
+#include "assert.h"
+
 // Determine whether or not we should wait for tasks in hybrid parallelism.
-bool should_task_wait(int mults_per_step, int total_rec_steps,
-					  int steps_left, int position, int num_threads) {
+bool should_task_wait(int mults_per_step, int total_rec_steps, int steps_left,
+					  int start_index, int position, int num_threads) {
+  assert(0 < position && position <= mults_per_step);
+
+#if defined(_PARALLEL_) && _PARALLEL_ == BFS_PAR
+  return position == mults_per_step;
+#endif
+
   int total_multiplies = pow(mults_per_step, total_rec_steps);
-  int extra_multiplies = num_threads % total_multiplies;
+  int extra_multiplies = total_multiplies % num_threads;
 
-  // TODO: fill in
-  int start_index = 0;
-
-  if (extra_multiplies == 0) {
-	return false;
+  if (position == mults_per_step) {
+	// The last position always waits.
+	return true;
   }
 
   int end_index = total_multiplies - extra_multiplies;
   // There is exactly one wait at the bottom level of the recursive tree.
   if (steps_left == 1 && start_index + position == end_index) {
+	//std::cout << "End stop: " << steps_left << " " << start_index << " " << position << std::endl;
 	return true;
   }
 
   int num_from_end = extra_multiplies / mults_per_step;
   if (steps_left == 2 && mults_per_step - num_from_end == position) {
+	//std::cout << "Early stop: " << steps_left << " " << start_index << " " << position << std::endl;
 	return true;
   }
 
   return false;
-  // Does the break fall in the middle of my recursive region?
 }
 
 // Determine whether or not we should launch a task for hybrid parallelism.
-bool should_launch_task(int mults_per_step, int total_rec_steps,
-						int steps_left, int position, int num_threads) {
-  return true;
-#if defined(_BFS_PAR_)
+bool should_launch_task(int mults_per_step, int total_rec_steps, int steps_left,
+						int start_index, int position, int num_threads) {
+  assert(0 < position && position <= mults_per_step);
+
+#if defined(_PARALLEL_) && _PARALLEL_ == BFS_PAR
   return true;
 #endif
 
   int total_multiplies = pow(mults_per_step, total_rec_steps);
-  int extra_multiplies = num_threads % total_multiplies;
-
-  // TODO: fill in
-  int start_index = 0;
-
-  if (extra_multiplies == 0) {
-	return true;
-  }
+  int extra_multiplies = total_multiplies % num_threads;
 
   int end_index = total_multiplies - extra_multiplies;
-  if (steps_left == 1 && start_index + position <= end_index) {
-	return true;
+  if (steps_left == 1 && start_index + position > end_index) {
+	//std::cout << "Not launching: " << steps_left << " " << start_index << " " << position << std::endl;
+	return false;
   }
   
   int num_from_end = extra_multiplies / mults_per_step;
-  if (steps_left == 2 && mults_per_step - num_from_end >= position) {
-	return true;
+  if (steps_left == 2 && mults_per_step - num_from_end < position) {
+	//std::cout << "Not launching: " << steps_left << " " << start_index << " " << position << std::endl;
+	return false;
   }
 
-  return false;
+  return true;
 }
+
+class Lock {
+ public:
+  Lock() { omp_init_lock(&lock_); }
+  ~Lock() { omp_destroy_lock(&lock_); }
+
+  void Acquire() { omp_set_lock(&lock_); }
+  void Release() { omp_unset_lock(&lock_); }
+
+ private:
+  omp_lock_t lock_;
+};
+
+
+class LockAndCounter {
+ public:
+  LockAndCounter(int count) : count_(count) { lock_.Acquire(); }
+
+  void decrement() {
+	#pragma omp atomic update
+	--count_;
+	if (count_ == 0) {
+	  lock_.Release();
+	}
+  }
+
+  Lock& lock() { return lock_; }
+  int count() { return count_; }
+
+ private:
+  int count_;
+  Lock lock_;
+};
 
 #endif  // _PAR_UTIL_
