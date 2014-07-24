83c9d6f98d8f16f4d84bca47539a57f477d5f81c
Austin Benson <arbenson@gmail.com>
2014-07-23 17:02:49 -0700

    Update for new parallelism definitions.

--- common.hpp
+++ common.hpp
@@ -2,11 +2,15 @@
 #define _COMMON_HPP_
 
 #include "linalg.hpp"
-#ifdef _CILK_
-# include <cilk/cilk.h>
-#elif defined(_BFS_PAR_) || defined(_DFS_PAR_) || defined(_HYBRID_PAR_)
+#ifdef _PARALLEL_
 # include <omp.h>
 # include "par_util.hpp"
 #endif
+
+#include <chrono>
+
+#define _DFS_PAR_ 1
+#define _BFS_PAR_ 2
+#define _HYBRID_PAR_ 3
 
 #endif  // _COMMON_HPP_
