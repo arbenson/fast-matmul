203e3b10b52f0c9575d240bcd6d81b2b971fc17d
Austin Benson <arbenson@gmail.com>
2014-08-01 09:52:35 -0700

    more blas routines

--- linalg/blas.hpp
+++ linalg/blas.hpp
@@ -12,6 +12,14 @@ extern "C" {
   void daxpy_(int *n, double *alpha, double *A, int *incx, double *C, int *incy);
   void saxpy_(int *n, float *alpha, float *A, int *incx, float *C, int *incy);
 
+  void dtrsm_(char *side, char *uplo, char *transa, char *diag, int *m, int *n,
+			  double *alpha, double *A, int *lda, double *B, int *ldb);
+  void strsm_(char *side, char *uplo, char *transa, char *diag, int *m, int *n,
+			  float *alpha, float *A, int *lda, float *B, int *ldb);
+
+
+  //  void dlaswp_(int *N, double *A, int *lda, int *K1, int *K2, int *ipiv, int *incx);
+  //  void slaswp_(int *N, float *A, int *lda, int *K1, int *K2, int *ipiv, int *incx);
 }
 
 #endif  // _BLAS_HPP_
