74055ca5943f946d6bcb8d402ddebc29e4bfa027
Austin Benson <arbenson@gmail.com>
2014-08-02 10:44:40 -0700

    Updates to fast lu and fast qr

--- examples/fast_lu.cpp
+++ examples/fast_lu.cpp
@@ -1,5 +1,6 @@
 #include "blas.hpp"
 #include "common.hpp"
+#include "lapack_wrap.hpp"
 #include "linalg.hpp"
 #include "mkl.h"
 #include "fast424_26_257.hpp"
@@ -9,38 +10,6 @@
 #include <stdexcept>
 #include <vector>
 
-void Getrf(double *data, int m, int n, int lda, int *pivots) {
-  int info;
-  dgetrf_(&m, &n, data, &lda, pivots, &info);
-  assert(info == 0);
-}
-
-void Getrf(float *data, int m, int n, int lda, int *pivots) {
-  int info;
-  sgetrf_(&m, &n, data, &lda, pivots, &info);
-  assert(info == 0);
-}
-
-void Laswp(int num_cols, double *data, int lda, int piv_start, int piv_end,
-		   int *pivots, int incx) {
-  dlaswp_(&num_cols, data, &lda,  &piv_start, &piv_end, pivots, &incx);
-}
-
-void Laswp(int num_cols, float *data, int lda, int piv_start, int piv_end,
-		   int *pivots, int incx) {
-  slaswp_(&num_cols, data, &lda,  &piv_start, &piv_end, pivots, &incx);
-}
-
-void Trsm(char side, char uplo, char transa, char diag, int m, int n,
-		  double alpha, double *A, int lda, double *B, int ldb) {
-  dtrsm_(&side, &uplo, &transa, &diag, &m, &n, &alpha, A, &lda, B, &ldb);
-}
-
-void Trsm(char side, char uplo, char transa, char diag, int m, int n,
-		  float alpha, float *A, int lda, float *B, int ldb) {
-  strsm_(&side, &uplo, &transa, &diag, &m, &n, &alpha, A, &lda, B, &ldb);
-}
-
 template<typename Scalar>
 void LU(Matrix<Scalar>& A, std::vector<int>& pivots, int pos) {
   assert(A.m() > 0 && A.n() > 0);
@@ -48,16 +17,18 @@ void LU(Matrix<Scalar>& A, std::vector<int>& pivots, int pos) {
   int n = A.n();
   int lda = A.stride();
   Scalar *data = A.data();
-  Getrf(data, m, n, lda, &pivots[pos]);
+  lapack::Getrf(data, m, n, lda, &pivots[pos]);
 }
+
 
 template<typename Scalar>
 void Pivot(Matrix<Scalar>& A, std::vector<int>& pivots, int start, int num_pivs) {
   int N = A.n();
   Scalar *data = A.data();
   int stride = A.stride();
-  Laswp(N, data, stride, start, start + num_pivs, &pivots[0], 1);
+  lapack::Laswp(N, data, stride, start, start + num_pivs, &pivots[0], 1);
 }
+
 
 // Solve LX = B and store X in B.  L is lower triangular.
 template<typename Scalar>
@@ -73,14 +44,14 @@ void TriangSolve(Matrix<Scalar>& L, Matrix<Scalar>& B) {
   int lda = L.stride();
   Scalar *data_B = B.data();
   int ldb = B.stride();
-  Trsm(side, uplo, transa, diag, M, N, alpha, data_A, lda, data_B, ldb);
+  lapack::Trsm(side, uplo, transa, diag, M, N, alpha, data_A, lda, data_B, ldb);
 }
 
 
 template<typename Scalar>
 void FastLU(Matrix<Scalar>& A, int blocksize) {
-  if (A.m() < A.n()) {
-	throw std::logic_error("Only supporting m >= n");
+  if (A.m() != A.n()) {
+	throw std::logic_error("Only supporting m == n");
   }
   std::vector<int> pivots(A.m());
   
@@ -105,7 +76,6 @@ void FastLU(Matrix<Scalar>& A, int blocksize) {
   // Now deal with the leftovers
   int start_ind = (A.m() / blocksize) * blocksize;
   int num_left = A.m() - start_ind;
-  assert(num_left >= A.m());
   if (num_left == 0) {
 	return;
   }
@@ -117,12 +87,11 @@ void FastLU(Matrix<Scalar>& A, int blocksize) {
 
 int main(int argc, char **argv) {
   int n = 10000;
+  int blocksize = 1600;
   Matrix<double> A = RandomMatrix<double>(n, n);
   Matrix<double> B = A;
 
-  omp_set_num_threads(7);
-
   std::vector<int> pivots(A.m());
   Time([&] { LU(A, pivots, 0); }, "Classical LU");
-  Time([&] { FastLU(B, 2400); }, "Fast LU");
+  Time([&] { FastLU(B, 1600); }, "Fast LU");
 }
--- examples/fast_qr.cpp
+++ examples/fast_qr.cpp
@@ -1,4 +1,5 @@
 #include "blas.hpp"
+#include "lapack_wrap.hpp"
 #include "linalg.hpp"
 #include "mkl.h"
 #include "strassen.hpp"
@@ -6,53 +7,6 @@
 
 #include <stdexcept>
 #include <vector>
-
-Geqrf(int m, int n, float *A, int lda, float *tau) {
-  // First perform the workspace query.
-  float *work = new float[1];
-  int lwork = -1;
-  int info;
-  sgeqrf_(&m, &n, A, &lda, tau, work, lwork, &info);
-  if (info != 0) {
-	throw std::exception("Bad sgeqrf_ call");
-  }
-  
-  // Now allocate the work array.
-  lwork = static_cast<int>(work[0]);
-  delete [] work;
-  work = new float[lwork];
-  
-  // QR routine that does the work.
-  sgeqrf_(&m, &n, A, &lda, tau, work, lwork, &info);  
-  if (info != 0) {
-	throw std::exception("Bad sgeqrf_ call");
-  }
-  delete [] work;
-}
-
-
-Geqrf(int m, int n, double *A, int lda, double *tau) {
-  // First perform the workspace query.
-  double *work = new double[1];
-  int lwork = -1;
-  int info;
-  dgeqrf_(&m, &n, A, &lda, tau, work, lwork, &info);
-  if (info != 0) {
-	throw std::exception("Bad dgeqrf_ call");
-  }
-  
-  // Now allocate the work array.
-  lwork = static_cast<int>(work[0]);
-  delete [] work;
-  work = new double[lwork];
-
-  // QR routine that does the work.
-  dgeqrf_(&m, &n, A, &lda, tau, work, lwork, &info);  
-  if (info != 0) {
-	throw std::exception("Bad dgeqrf_ call");
-  }
-  delete [] work;
-}
 
 
 template <typename Scalar>
@@ -62,18 +16,9 @@ void QR (Matrix<Scalar>& A, std::vector<Scalar>& tau, int pos) {
   int lda = A.stride();
   Scalar *A_data = A.data();
   Scalar *curr_tau = &tau[pos];
-  Geqrf(m, n, A_data, lda, curr_tau);
+  lapack::Geqrf(m, n, A_data, lda, curr_tau);
 }
 
-void Larft(char direct, char storev, int m, int n, double *V, int ldv,
-		   double *tau, double *T, int ldt) {
-  dlarft_(&direct, &storev, &m, &n, V, &ldv, tau, T, &ldt);
-}
-
-void Larft(char direct, char storev, int m, int n, float *V, int ldv,
-		   float *tau, float *T, int ldt) {
-  slarft_(&direct, &storev, &m, &n, V, &ldv, tau, T, &ldt);
-}
 
 template <typename Scalar>
 Matrix<Scalar> TriangFactor(Matrix<Scalar>& A, std::vector<Scalar>& tau,
@@ -84,59 +29,89 @@ Matrix<Scalar> TriangFactor(Matrix<Scalar>& A, std::vector<Scalar>& tau,
   int M = A.m();
   int N = A.n();
   int lda = A.stride();
-  Scalar *tau = &tau[pos];
+  Scalar *curr_tau = &tau[pos];
 
   // Triangular matrix for storing data.
   int ldt = N;
   Matrix<Scalar> T(ldt, N);
   Scalar *T_data = T.data();
-
-  Larft(direct, storev, M, N, A_data, lda, tau, T_data, ldt);
+  lapack::Larft(direct, storev, M, N, A_data, lda, curr_tau, T_data, ldt);
 
   return T;
 }
 
 
+template <typename Scalar>
+void TriangMatmul(char side, char uplo, char transt, char diag,
+				  Scalar alpha, Matrix<Scalar>& T, Matrix<Scalar>& B) {
+  int m = B.m();
+  int n = B.n();
+  Scalar *T_data = T.data();
+  int ldt = T.stride();
+  Scalar *B_data = B.data();
+  int ldb = B.stride();
+  lapack::Trmm(side, uplo, transt, diag, m, n, alpha, T_data, ldt, B_data, ldb);
+}
+
+
+template <typename Scalar>
+void UpdateTrailing(Matrix<Scalar>& V, Matrix<Scalar>& T, Matrix<Scalar>& A2) {
+  // W := V^T
+  Matrix<Scalar> Vt = TransposedCopy(V);
+
+  // W := Vt * A2
+  Matrix<Scalar> W(Vt.m(), A2.n());
+  int num_steps = 1;
+  strassen::FastMatmul(Vt, A2, W, num_steps);
+
+  // W := T^T * W
+  TriangMatmul('L', 'U', 'T', 'N', Scalar(1.0), T, W);
+
+  // A2 := A2 - VW
+  strassen::FastMatmul(V, W, A2, num_steps, 0.0, -1.0, 1.0);
+}
+
+
 template<typename Scalar>
 void FastQR(Matrix<Scalar>& A, std::vector<Scalar>& tau, int blocksize) {
-  if (A.m() < A.n()) {
-	throw std::logic_error("Only supporting m >= n");
+  if (A.m() != A.n()) {
+	throw std::logic_error("Only supporting m == n for now.");
   }
   tau.resize(A.n());
   
   for (int i = 0; i < A.m() && A.m() - i > blocksize; i += blocksize) {
-	// Split A as
-	//     A = (A1 A2)
-	// and compute QR on A1
+	// Split A as A = (A1 A2) and compute QR on A1.
 	Matrix<Scalar> A1 = A.Submatrix(i, i, A.m() - i, blocksize);
 	Matrix<Scalar> A2 = A.Submatrix(i, i + blocksize, A.m() - i, A.n() - i - blocksize);
 	QR(A1, tau, i);
 
 	// Form the triangular factor of the reflectors
-	Matrix<Scalar> T = TriangFactor(A1, tau, pos);
+	Matrix<Scalar> T = TriangFactor(A1, tau, i);
 
+	// A2 := (I - VT'V')A2
+	UpdateTrailing(A1, T, A2);
   }
+
   // Now deal with the leftovers
   int start_ind = (A.m() / blocksize) * blocksize;
   int num_left = A.m() - start_ind;
-  assert(num_left >= A.m());
   if (num_left == 0) {
 	return;
   }
   Matrix<Scalar> A_end = A.Submatrix(start_ind, start_ind, num_left, A.n() - start_ind);
-  LU(A_end, pivots, start_ind);
-  Pivot(A,  pivots, start_ind, num_left);
+  QR(A_end, tau, start_ind);
 }
 
 
 int main(int argc, char **argv) {
-  int n = 10000;
+  int n = 8000;
+  int blocksize = 4000;
   Matrix<double> A = RandomMatrix<double>(n, n);
   Matrix<double> B = A;
 
   std::vector<double> tau(A.n());
-  Time([&] { QR(A, pivots, 0); }, "Classical QR");
-  Time([&] { FastQR(B, 2400); }, "Fast QR");
+  Time([&] { QR(A, tau, 0); }, "Classical QR");
+  Time([&] { FastQR(B, tau, blocksize); }, "Fast QR");
 
   return 0;
 }
