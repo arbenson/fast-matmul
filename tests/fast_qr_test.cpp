a3a4a509b8388ba2969ba722566596c760514667
Austin Benson <arbenson@gmail.com>
2014-08-04 11:21:44 -0700

    Infrastructure for testing QR.  Segfaulting currently (probably issue with lapack calls)

--- tests/fast_qr_test.cpp
+++ tests/fast_qr_test.cpp
@@ -1,36 +1,54 @@
 #include "common.hpp"
-#include "fast_lu.hpp"
+#include "fast_qr.hpp"
 #include "lapack_wrap.hpp"
 #include "linalg.hpp"
 
 #include <vector>
 
-void Compare(Matrix<double>& A, std::vector<int>& pivots_A,
-			 Matrix<double>& B, std::vector<int>& pivots_B) {
+void QRSolve(Matrix<double>& A, std::vector<double>& tau, Matrix<double>& RHS) {
+  assert(A.m() == RHS.m());
+  char side = 'L';   // Apply Q' on left-hand-side
+  char trans = 'T';  // Apply Q'
+  // Apply Q' to RHS and store in RHS
+  lapack::Ormqr(side, trans, A.m(), A.n(), tau.size(), A.data(), A.stride(),
+				&tau[0], RHS.data(), RHS.stride());
+
+  // Solve with R and store in RHS
+  side = 'L';         // Apply on left-hand-side
+  char uplo = 'U';    // R is uppper triangular
+  char transa = 'N';  // no transpose
+  char diag = 'N';    // not unit triangular
+  lapack::Trsm(side, uplo, transa, diag, RHS.m(), RHS.n(), 1.0,
+			   A.data(), A.stride(), RHS.data(), RHS.stride());
+}
+
+
+void Compare(Matrix<double>& A, std::vector<double>& tau_A,
+			 Matrix<double>& B, std::vector<double>& tau_B) {
   Matrix<double> rhs_A = RandomMatrix<double>(A.m(), 1);
   Matrix<double> rhs_B = rhs_A;
-  lapack::Getrs('N', A.m(), 1, A.data(), A.stride(), &pivots_A[0],
-				rhs_A.data(), rhs_A.stride());
-  lapack::Getrs('N', B.m(), 1, B.data(), B.stride(), &pivots_B[0],
-				rhs_B.data(), rhs_B.stride());
+  QRSolve(A, tau_A, rhs_A);
+  QRSolve(B, tau_B, rhs_B);
   std::cout << "Max. rel. diff. (solution): " << MaxRelativeDiff(rhs_A, rhs_B)
 			<< std::endl;
 }
 
+
 void RunTest(int m, int n, int blocksize) {
-  std::cout << "Factoring " << m << " x " << m << " matrix with blocksize="
+  std::cout << "Factoring " << m << " x " << n << " matrix with blocksize="
 			<< blocksize << std::endl;
   Matrix<double> A = RandomMatrix<double>(m, n);
   Matrix<double> B = A;
-  LU(A, pivots_A);
-  FastLU(B, blocksize, pivots_B);
-  Compare(A, pivots_A, B, pivots_B);
+  std::vector<double> tau_A(A.n()), tau_B(B.n());
+  QR(A, tau_A);
+  FastQR(B, tau_B, blocksize);
+  Compare(A, tau_A, B, tau_B);
 }
 
 
 int main(int argc, char **argv) {
-  RunTest(4000, 800);
-  RunTest(5000, 1000);
-  RunTest(4301, 725);
+  RunTest(4000, 3000, 800);
+  RunTest(5000, 4000, 1000);
+  RunTest(4301, 3407, 725);
   return 0;
 }
