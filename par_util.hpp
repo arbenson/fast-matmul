62bb2d4ef3411751a6b9b2b44d29658f6341691d
Austin Benson <arbenson@gmail.com>
2014-08-11 14:47:58 -0700

    Change par_util to distinguish between BFS and HYBRID when there are fewer multiples than threads.

--- par_util.hpp
+++ par_util.hpp
@@ -1,6 +1,7 @@
 #ifndef _PAR_UTIL_
 #define _PAR_UTIL_
 
+#include "common.hpp"
 #include "math.h"
 #include "assert.h"
 
@@ -9,7 +10,7 @@ bool should_task_wait(int mults_per_step, int total_rec_steps, int steps_left,
 					  int start_index, int position, int num_threads) {
   assert(0 < position && position <= mults_per_step);
 
-#if defined(_PARALLEL_) && _PARALLEL_ == BFS_PAR
+#if defined(_PARALLEL_) && (_PARALLEL_ == _BFS_PAR_)
   return position == mults_per_step;
 #endif
 
@@ -42,12 +43,18 @@ bool should_launch_task(int mults_per_step, int total_rec_steps, int steps_left,
 						int start_index, int position, int num_threads) {
   assert(0 < position && position <= mults_per_step);
 
-#if defined(_PARALLEL_) && _PARALLEL_ == BFS_PAR
+#if defined(_PARALLEL_) && (_PARALLEL_ == _BFS_PAR_)
   return true;
 #endif
 
   int total_multiplies = pow(mults_per_step, total_rec_steps);
   int extra_multiplies = total_multiplies % num_threads;
+
+  // If the number of threads is greater than the total number of multiplies,
+  // then we just run parallel MKL.
+  if (num_threads > total_multiplies) {
+	return false;
+  }
 
   int end_index = total_multiplies - extra_multiplies;
   if (steps_left == 1 && start_index + position > end_index) {
