aa502612f2816c297ab13d0d4755382bd98695f5
Austin Benson <arbenson@gmail.com>
2014-08-11 13:03:22 -0700

    Make defines first.

--- common.hpp
+++ common.hpp
@@ -1,6 +1,10 @@
 #ifndef _COMMON_HPP_
 #define _COMMON_HPP_
 
+#define _DFS_PAR_ 1
+#define _BFS_PAR_ 2
+#define _HYBRID_PAR_ 3
+
 #include "linalg.hpp"
 #ifdef _PARALLEL_
 # include <omp.h>
@@ -9,10 +13,6 @@
 
 #include "options.hpp"
 #include "timing.hpp"
-
-#define _DFS_PAR_ 1
-#define _BFS_PAR_ 2
-#define _HYBRID_PAR_ 3
 
 template <typename T>
 std::ostream& operator<<(std::ostream& os, std::vector<T>& vec) {
