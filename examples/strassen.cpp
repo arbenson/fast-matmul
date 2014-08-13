9816a3911213a776330ad4d37a3d53d53e0d67a8
Austin Benson <arbenson@berkeley.edu>
2014-08-13 09:05:38 -0700

    merge

--- examples/strassen.cpp
+++ examples/strassen.cpp
@@ -5,20 +5,22 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-    int m = 8000;
-    int k = 8000;
-    int n = 8000;
-    int numsteps = 2;
+  int m = 10000;
+  int k = 10000;
+  int n = 10000;
+  int numsteps = 2;
 
-    Matrix<double> A = RandomMatrix<double>(m, k);
-    Matrix<double> B = RandomMatrix<double>(k, n);
-    Matrix<double> C1(m, n), C2(m, n);
+  Matrix<double> A = RandomMatrix<double>(m, k);
+  Matrix<double> B = RandomMatrix<double>(k, n);
+  Matrix<double> C1(m, n), C2(m, n);
 
-    Time([&] { MatMul(A, B, C1); }, "Classical gemm");
-	Time([&] { strassen::FastMatmul(A, B, C2, numsteps); }, "Strassen");
-
-    // Test for correctness.
-    std::cout << "Maximum relative difference: " << MaxRelativeDiff(C1, C2) << std::endl;
-
-    return 0;
+  mkl_set_num_threads(24);
+  
+  Time([&] { Gemm(A, B, C1); }, "Classical gemm");
+  Time([&] { strassen::FastMatmul(A, B, C2, numsteps); }, "Strassen");
+  
+  // Test for correctness.
+  std::cout << "Maximum relative difference: " << MaxRelativeDiff(C1, C2) << std::endl;
+  
+  return 0;
 }
