439ac03b95bde67af9f043c87991f357e424e67c
Austin Benson <arbenson@berkeley.edu>
2014-08-20 12:10:24 -0700

    fix spacing

--- linalg/linalg.hpp
+++ linalg/linalg.hpp
@@ -18,6 +18,8 @@
 #include <cmath>
 #include <iostream>
 
+#include "timing.hpp"
+
 // This is a basic templated matrix class.
 // It stores the matrix data, the stride, and the dimensions.
 // The variable is_view indicates whether or not the data is owned
@@ -27,10 +29,10 @@ class Matrix {
 public:
 
   Matrix(Scalar *data, int stride, int m, int n, Scalar multiplier, bool is_view) :
-	data_(data), stride_(stride), m_(m), n_(n), multiplier_(multiplier), is_view_(is_view) {
-	if (!is_view) {
-	  allocate();
-	}
+    data_(data), stride_(stride), m_(m), n_(n), multiplier_(multiplier), is_view_(is_view) {
+    if (!is_view) {
+      allocate();
+    }
   }  
 
   Matrix(Scalar *data, int stride, int m, int n, Scalar multiplier) :
@@ -43,35 +45,35 @@ public:
 
   // Copy constructor
   Matrix(Matrix<Scalar>& that) : Matrix(NULL, that.m(), that.m(), that.n(),
-										that.multiplier(), false) {
-	Scalar *that_data = that.data();
-	int that_stride = that.stride();
-	for (int j = 0; j < n_; ++j) {
-	  for (int i = 0; i < m_; ++i) {
-		data_[i + j * stride_] = that_data[i + j * that_stride];
-	  }
-	}
+                                        that.multiplier(), false) {
+    Scalar *that_data = that.data();
+    int that_stride = that.stride();
+    for (int j = 0; j < n_; ++j) {
+      for (int i = 0; i < m_; ++i) {
+        data_[i + j * stride_] = that_data[i + j * that_stride];
+      }
+    }
   }
 
   // Move constructor
   Matrix(Matrix<Scalar>&& that) : Matrix() {
-	swap(*this, that);
+    swap(*this, that);
   }
 
   // copy assignment
   Matrix<Scalar>& operator=(Matrix<Scalar> that) {
-	swap(*this, that);
-	return *this;
+    swap(*this, that);
+    return *this;
   }
 
   friend void swap(Matrix<Scalar>& first, Matrix<Scalar>& second) {
-	using std::swap;
-	swap(first.m_, second.m_);
-	swap(first.n_, second.n_);
-	swap(first.stride_, second.stride_);
-	swap(first.is_view_, second.is_view_);
-	swap(first.data_, second.data_);
-	swap(first.multiplier_, second.multiplier_);
+    using std::swap;
+    swap(first.m_, second.m_);
+    swap(first.n_, second.n_);
+    swap(first.stride_, second.stride_);
+    swap(first.is_view_, second.is_view_);
+    swap(first.data_, second.data_);
+    swap(first.multiplier_, second.multiplier_);
   }
 
 
@@ -79,23 +81,23 @@ public:
   // num_block_rows and num_block_cols are the number of block rows and columns
   // row_ind and col_ind are the indices (1-indexed) of the block
   Matrix<Scalar> Subblock(int num_block_rows, int num_block_cols, int row_ind,
-						  int col_ind) {
-	std::pair<int, int> row_data = IndexData(m_, num_block_rows, row_ind);
-	std::pair<int, int> col_data = IndexData(n_, num_block_cols, col_ind);
-	return Submatrix(row_data.first, col_data.first, row_data.second, col_data.second);
+                          int col_ind) {
+    std::pair<int, int> row_data = IndexData(m_, num_block_rows, row_ind);
+    std::pair<int, int> col_data = IndexData(n_, num_block_cols, col_ind);
+    return Submatrix(row_data.first, col_data.first, row_data.second, col_data.second);
   }
 
 
   Matrix<Scalar> Submatrix(int start_row, int start_col, int num_rows,
-						   int num_cols) {
-	return Matrix<Scalar>(data(start_row, start_col), stride_, num_rows, num_cols, multiplier_);
+                           int num_cols) {
+    return Matrix<Scalar>(data(start_row, start_col), stride_, num_rows, num_cols, multiplier_);
   }
 
 
   ~Matrix() {
-	if (data_ != NULL && !is_view_ && m_ > 0 && n_ > 0) {
-	  deallocate();
-	}
+    if (data_ != NULL && !is_view_ && m_ > 0 && n_ > 0) {
+      deallocate();
+    }
   }
 
   Scalar *data() { return data_; }
@@ -106,28 +108,28 @@ public:
 
 
   void allocate() {
-	if (n_ > 0 && m_ > 0) {
-	  assert(stride_ >= m_);
+    if (n_ > 0 && m_ > 0) {
+      assert(stride_ >= m_);
 #ifdef __INTEL_MKL__
-	  int alignment = 32;
-	  data_ = static_cast<Scalar *>(mkl_malloc(sizeof(Scalar) * m_ * n_, alignment));
+      int alignment = 32;
+      data_ = static_cast<Scalar *>(mkl_malloc(sizeof(Scalar) * m_ * n_, alignment));
 #else
-	  data_ = new Scalar[m_ * n_];
+      data_ = new Scalar[m_ * n_];
 #endif
-	  assert(data_ != NULL);
-	}
+      assert(data_ != NULL);
+    }
   }
 
 
   void deallocate() {
-	if (data_ != NULL) {
+    if (data_ != NULL) {
 #ifdef __INTEL_MKL__
-	  mkl_free(data_);
+      mkl_free(data_);
 #else
-	  delete[] data_;
+      delete[] data_;
 #endif
-	  data_ = NULL;
-	}
+      data_ = NULL;
+    }
   }
 
   void UpdateMultiplier(Scalar multiplier) { multiplier_ *= multiplier; }
@@ -137,11 +139,10 @@ public:
 private:
   // Return pair of (starting index, number of indices)
   std::pair<int, int> IndexData(int total, int num_block, int ind) {
-  	int step = total / num_block;
-	// Determine starting index
-	int start = step * (ind - 1);
-
-	return std::pair<int, int>(start, step);
+    int step = total / num_block;
+    // Determine starting index
+    int start = step * (ind - 1);
+    return std::pair<int, int>(start, step);
   }
 
   Scalar *data_;
@@ -161,7 +162,7 @@ private:
 // the fast algorithm (M x K times K x N base case).
 template<typename Scalar>
 void DynamicPeeling(Matrix<Scalar>& A, Matrix<Scalar>& B, Matrix<Scalar>& C,
-					int dim1, int dim2, int dim3, Scalar beta) {
+                    int dim1, int dim2, int dim3, Scalar beta) {
   assert(A.m() == C.m() && A.n() == B.m() && B.n() == C.n());
   assert(A.m() > 0 && A.n() > 0);
   int extra_rows_A = A.m() - (A.m() / dim1) * dim1;
@@ -173,34 +174,34 @@ void DynamicPeeling(Matrix<Scalar>& A, Matrix<Scalar>& B, Matrix<Scalar>& C,
   // Adjust part handled by fast matrix multiplication.
   // Add far column of A outer product bottom row B
   if (extra_cols_A > 0) {
-	// In Strassen, this looks like C([1, 2], [1, 2]) += A([1, 2], 3) * B(3, [1, 2])
-	int row_dim = A.m() - extra_rows_A;
-	int col_dim = B.n() - extra_cols_B;
-	int inner_dim_start = A.n() - extra_cols_A;
-	Matrix<Scalar> A_extra = A.Submatrix(0, inner_dim_start, row_dim, extra_cols_A);
-	Matrix<Scalar> B_extra = B.Submatrix(inner_dim_start, 0, extra_rows_B, col_dim);
-	Matrix<Scalar> C_extra = C.Submatrix(0, 0, row_dim, col_dim);
-	MatMul(A_extra, B_extra, C_extra, Scalar(1.0));
+    // In Strassen, this looks like C([1, 2], [1, 2]) += A([1, 2], 3) * B(3, [1, 2])
+    int row_dim = A.m() - extra_rows_A;
+    int col_dim = B.n() - extra_cols_B;
+    int inner_dim_start = A.n() - extra_cols_A;
+    Matrix<Scalar> A_extra = A.Submatrix(0, inner_dim_start, row_dim, extra_cols_A);
+    Matrix<Scalar> B_extra = B.Submatrix(inner_dim_start, 0, extra_rows_B, col_dim);
+    Matrix<Scalar> C_extra = C.Submatrix(0, 0, row_dim, col_dim);
+    MatMul(A_extra, B_extra, C_extra, Scalar(1.0));
   }
 
   // Adjust for far right columns of C
   if (extra_cols_B > 0) {
-	// In Strassen, this looks like C(:, 3) = A * B(:, 3)
-	int start_ind = B.n() - extra_cols_B;
-	Matrix<Scalar> B_extra = B.Submatrix(0, start_ind, B.m(), extra_cols_B);
-	Matrix<Scalar> C_extra = C.Submatrix(0, start_ind, C.m(), extra_cols_B);
-	MatMul(A, B_extra, C_extra, beta);
+    // In Strassen, this looks like C(:, 3) = A * B(:, 3)
+    int start_ind = B.n() - extra_cols_B;
+    Matrix<Scalar> B_extra = B.Submatrix(0, start_ind, B.m(), extra_cols_B);
+    Matrix<Scalar> C_extra = C.Submatrix(0, start_ind, C.m(), extra_cols_B);
+    MatMul(A, B_extra, C_extra, beta);
   }
 
   // Adjust for bottom rows of C
   if (extra_rows_A > 0) {
-	// In Strassen, this looks like C(3, [1, 2]) = A(3, :) * B(:, [1, 2])
-	int start_ind = A.m() - extra_rows_A;
-	int num_cols = B.n() - extra_cols_B;
-	Matrix<Scalar> A_extra = A.Submatrix(start_ind, 0, extra_rows_A, A.n());
-	Matrix<Scalar> B_extra = B.Submatrix(0, 0, B.m(), num_cols);
-	Matrix<Scalar> C_extra = C.Submatrix(start_ind, 0, extra_rows_A, num_cols);
-	MatMul(A_extra, B_extra, C_extra, beta);
+    // In Strassen, this looks like C(3, [1, 2]) = A(3, :) * B(:, [1, 2])
+    int start_ind = A.m() - extra_rows_A;
+    int num_cols = B.n() - extra_cols_B;
+    Matrix<Scalar> A_extra = A.Submatrix(start_ind, 0, extra_rows_A, A.n());
+    Matrix<Scalar> B_extra = B.Submatrix(0, 0, B.m(), num_cols);
+    Matrix<Scalar> C_extra = C.Submatrix(start_ind, 0, extra_rows_A, num_cols);
+    MatMul(A_extra, B_extra, C_extra, beta);
   }
 }
 
@@ -211,10 +212,10 @@ std::ostream& operator<<(std::ostream& os, Matrix<Scalar>& mat) {
   Scalar *data = mat.data();
   int stride = mat.stride();
   for (int i = 0; i < mat.m(); ++i) {
-	for (int j = 0; j < mat.n(); ++j) {
-	  os << data[i + j * stride] << " ";
-	}
-	os << std::endl;
+    for (int j = 0; j < mat.n(); ++j) {
+      os << data[i + j * stride] << " ";
+    }
+    os << std::endl;
   }
   return os;
 }
@@ -223,12 +224,12 @@ std::ostream& operator<<(std::ostream& os, Matrix<Scalar>& mat) {
 // C <-- A * B + beta * C
 template <typename Scalar>
 void MatMul(Matrix<Scalar>& A, Matrix<Scalar>& B, Matrix<Scalar>& C,
-			Scalar beta=Scalar(0.0)) {
+            Scalar beta=Scalar(0.0)) {
   assert(A.m() == C.m() && A.n() == B.m() && B.n() == C.n());
   assert(A.m() > 0 && A.n() > 0);
   Scalar alpha = C.multiplier();
   lapack::Gemm('N', 'N', A.m(), B.n(), A.n(), A.data(), A.stride(), B.data(),
-			   B.stride(), C.data(), C.stride(), alpha, beta);
+               B.stride(), C.data(), C.stride(), alpha, beta);
 }
 
 // C <-- alpha * A + C.  n is the number of entries.
@@ -248,14 +249,14 @@ double MaxRelativeDiff(Matrix<Scalar>& A, Matrix<Scalar>& B) {
   const Scalar *dataB = B.data();
   double max_rel_diff = 0;
   for (int j = 0; j < A.n(); ++j) {
-	for (int i = 0; i < A.m(); ++i) {
-	  Scalar a = dataA[i + j * strideA];
-	  Scalar b = dataB[i + j * strideB];
-	  Scalar curr_rel_diff = std::abs(a - b) / std::abs(a);
-	  if (curr_rel_diff > max_rel_diff) {
-		max_rel_diff = curr_rel_diff;
-	  }
-	}
+    for (int i = 0; i < A.m(); ++i) {
+      Scalar a = dataA[i + j * strideA];
+      Scalar b = dataB[i + j * strideB];
+      Scalar curr_rel_diff = std::abs(a - b) / std::abs(a);
+      if (curr_rel_diff > max_rel_diff) {
+        max_rel_diff = curr_rel_diff;
+      }
+    }
   }
   return max_rel_diff;
 }
@@ -271,12 +272,12 @@ double FrobeniusDiff(Matrix<Scalar>& A, Matrix<Scalar>& B) {
   const Scalar *dataA = A.data();
   const Scalar *dataB = B.data();
   for (int j = 0; j < A.n(); ++j) {
-	for (int i = 0; i < A.m(); ++i) {
-	  Scalar a = dataA[i + j * strideA];
-	  Scalar b = dataB[i + j * strideB];
-	  Scalar local_diff = (a - b);
-	  diff += local_diff * local_diff;
-	}
+    for (int i = 0; i < A.m(); ++i) {
+      Scalar a = dataA[i + j * strideA];
+      Scalar b = dataB[i + j * strideB];
+      Scalar local_diff = (a - b);
+      diff += local_diff * local_diff;
+    }
   }
   return sqrt(diff);
 }
@@ -289,10 +290,10 @@ double FrobeniusNorm(Matrix<Scalar>& A) {
   const int strideA = A.stride();
   const Scalar *dataA = A.data();
   for (int j = 0; j < A.n(); ++j) {
-	for (int i = 0; i < A.m(); ++i) {
-	  Scalar a = dataA[i + j * strideA];
-	  norm += a * a;
-	}
+    for (int i = 0; i < A.m(); ++i) {
+      Scalar a = dataA[i + j * strideA];
+      norm += a * a;
+    }
   }
   return sqrt(norm);
 }
@@ -310,10 +311,10 @@ void Negate(Matrix<Scalar>& A, Matrix<Scalar>& C) {
 # pragma omp parallel for collapse(2)
 #endif
   for (int j = 0; j < C.n(); ++j) {
-	for (int i = 0; i < C.m(); ++i) {
-	  Scalar a = dataA[i + j * strideA];
-	  dataC[i + j * strideC] = -a;
-	}
+    for (int i = 0; i < C.m(); ++i) {
+      Scalar a = dataA[i + j * strideA];
+      dataC[i + j * strideC] = -a;
+    }
   }
 }
 
@@ -330,10 +331,10 @@ void Copy(Matrix<Scalar>& A, Matrix<Scalar>& C) {
 # pragma omp parallel for collapse(2)
 #endif
   for (int j = 0; j < C.n(); ++j) {
-	for (int i = 0; i < C.m(); ++i) {
-	  Scalar a = dataA[i + j * strideA];
-	  dataC[i + j * strideC] = a;
-	}
+    for (int i = 0; i < C.m(); ++i) {
+      Scalar a = dataA[i + j * strideA];
+      dataC[i + j * strideC] = a;
+    }
   }
 }
 
@@ -341,115 +342,115 @@ void Copy(Matrix<Scalar>& A, Matrix<Scalar>& C) {
 // C = alpha1 * A1
 template <typename Scalar>
 void Copy(Matrix<Scalar>& A1,
-		  Scalar alpha1,
-		  Matrix<Scalar>& C) {
-    const int strideA1 = A1.stride();
-    Scalar *dataA1 = A1.data();
+          Scalar alpha1,
+          Matrix<Scalar>& C) {
+  const int strideA1 = A1.stride();
+  Scalar *dataA1 = A1.data();
 
-    const int strideC = C.stride();
-    Scalar *dataC = C.data();
+  const int strideC = C.stride();
+  Scalar *dataC = C.data();
 
-    for (int j = 0; j < C.n(); ++j) {
-        const Scalar *dataA_curr = dataA1 + j * strideA1;
-		Scalar *dataC_curr = dataC + j * strideC;
-        for (int i = 0; i < C.m(); ++i) {
-            dataC_curr[i] = alpha1 * dataA_curr[i];
-        }
+  for (int j = 0; j < C.n(); ++j) {
+    const Scalar *dataA_curr = dataA1 + j * strideA1;
+    Scalar *dataC_curr = dataC + j * strideC;
+    for (int i = 0; i < C.m(); ++i) {
+      dataC_curr[i] = alpha1 * dataA_curr[i];
     }
+  }
 }
 
 
 // C += alpha1 * A1
 template <typename Scalar>
 void UpdateAdd(Matrix<Scalar>& A1,
-			   Scalar alpha1,
-			   Matrix<Scalar>& C) {
-    const int strideA1 = A1.stride();
-    Scalar *dataA1 = A1.data();
+               Scalar alpha1,
+               Matrix<Scalar>& C) {
+  const int strideA1 = A1.stride();
+  Scalar *dataA1 = A1.data();
 
-    const int strideC = C.stride();
-    Scalar *dataC = C.data();
+  const int strideC = C.stride();
+  Scalar *dataC = C.data();
 
-    for (int j = 0; j < C.n(); ++j) {
-        const Scalar *dataA_curr = dataA1 + j * strideA1;
-		Scalar *dataC_curr = dataC + j * strideC;
-        for (int i = 0; i < C.m(); ++i) {
-            dataC_curr[i] += alpha1 * dataA_curr[i];
-        }
+  for (int j = 0; j < C.n(); ++j) {
+    const Scalar *dataA_curr = dataA1 + j * strideA1;
+    Scalar *dataC_curr = dataC + j * strideC;
+    for (int i = 0; i < C.m(); ++i) {
+      dataC_curr[i] += alpha1 * dataA_curr[i];
     }
+  }
 }
 
 
 // C += A1
 template <typename Scalar>
 void UpdateAdd(Matrix<Scalar>& A1,
-			   Matrix<Scalar>& C) {
-    const int strideA1 = A1.stride();
-    Scalar *dataA1 = A1.data();
+               Matrix<Scalar>& C) {
+  const int strideA1 = A1.stride();
+  Scalar *dataA1 = A1.data();
 
-    const int strideC = C.stride();
-    Scalar *dataC = C.data();
+  const int strideC = C.stride();
+  Scalar *dataC = C.data();
 
-    for (int j = 0; j < C.n(); ++j) {
-        const Scalar *dataA_curr = dataA1 + j * strideA1;
-		Scalar *dataC_curr = dataC + j * strideC;
-        for (int i = 0; i < C.m(); ++i) {
-            dataC_curr[i] += dataA_curr[i];
-        }
+  for (int j = 0; j < C.n(); ++j) {
+    const Scalar *dataA_curr = dataA1 + j * strideA1;
+    Scalar *dataC_curr = dataC + j * strideC;
+    for (int i = 0; i < C.m(); ++i) {
+      dataC_curr[i] += dataA_curr[i];
     }
+  }
 }
 
 
 // C -= A1
 template <typename Scalar>
 void UpdateSub(Matrix<Scalar>& A1,
-			   Matrix<Scalar>& C) {
-    const int strideA1 = A1.stride();
-    Scalar *dataA1 = A1.data();
+               Matrix<Scalar>& C) {
+  const int strideA1 = A1.stride();
+  Scalar *dataA1 = A1.data();
 
-    const int strideC = C.stride();
-    Scalar *dataC = C.data();
+  const int strideC = C.stride();
+  Scalar *dataC = C.data();
 
-    for (int j = 0; j < C.n(); ++j) {
-        const Scalar *dataA_curr = dataA1 + j * strideA1;
-		Scalar *dataC_curr = dataC + j * strideC;
-        for (int i = 0; i < C.m(); ++i) {
-            dataC_curr[i] -= dataA_curr[i];
-        }
+  for (int j = 0; j < C.n(); ++j) {
+    const Scalar *dataA_curr = dataA1 + j * strideA1;
+    Scalar *dataC_curr = dataC + j * strideC;
+    for (int i = 0; i < C.m(); ++i) {
+      dataC_curr[i] -= dataA_curr[i];
     }
+  }
 }
 
 
 // C += alpha1 * A1
 template <typename Scalar>
 void UpdateAddDaxpy(Matrix<Scalar>& A1,
-					Scalar alpha1,
-					Matrix<Scalar>& C) {
-    const int strideA1 = A1.stride();
-    Scalar *dataA1 = A1.data();
+                    Scalar alpha1,
+                    Matrix<Scalar>& C) {
+  const int strideA1 = A1.stride();
+  Scalar *dataA1 = A1.data();
 
-    const int strideC = C.stride();
-    Scalar *dataC = C.data();
+  const int strideC = C.stride();
+  Scalar *dataC = C.data();
 
-    for (int j = 0; j < C.n(); ++j) {
-        Scalar *dataA_curr = dataA1 + j * strideA1;
-		Scalar *dataC_curr = dataC + j * strideC;
-		AxpyWrap(dataC_curr, dataA_curr, C.m(), alpha1);
-    }
+  for (int j = 0; j < C.n(); ++j) {
+    Scalar *dataA_curr = dataA1 + j * strideA1;
+    Scalar *dataC_curr = dataC + j * strideC;
+    AxpyWrap(dataC_curr, dataA_curr, C.m(), alpha1);
+  }
 }
 
 
 // C := 0
 template <typename Scalar>
 void ZeroOut(Matrix<Scalar>& C) {
-    const int strideC = C.stride();
-    Scalar *dataC = C.data();
+  const int strideC = C.stride();
+  Scalar *dataC = C.data();
 
-    for (int j = 0; j < C.n(); ++j) {
-        for (int i = 0; i < C.m(); ++i) {
-		    dataC[i + j * strideC] = Scalar(0);
-        }
+  for (int j = 0; j < C.n(); ++j) {
+    for (int i = 0; i < C.m(); ++i) {
+      dataC[i + j * strideC] = Scalar(0);
     }
+  }
 }
 
 
@@ -461,7 +462,7 @@ Matrix<Scalar> RandomMatrix(int m, int n) {
   // still slow on some systems.
   for (int j = 0; j < A.n(); ++j) {
     for (int i = 0; i < A.m(); ++i) {
-	  double val = static_cast<double>(rand()) / RAND_MAX;
+      double val = static_cast<double>(rand()) / RAND_MAX;
       A.data()[i + j * A.stride()] = val;
     }
   }  
@@ -478,9 +479,9 @@ Matrix<Scalar> TransposedCopy(Matrix<Scalar>& A) {
   int A_stride = A.stride();
   int At_stride = At.stride();
   for (int j = 0; j < A.n(); ++j) {
-	for (int i = 0; i < A.m(); ++i) {
-	  At_data[j + i * At_stride] = A_data[i + j * A_stride];
-	}
+    for (int i = 0; i < A.m(); ++i) {
+      At_data[j + i * At_stride] = A_data[i + j * A_stride];
+    }
   }
   return At;
 }
