b8581599fd2f300ccadbdd48f39de1f772861323
Grey Ballard <gmballa@sandia.gov>
2014-10-03 14:09:20 -0700

    minor changes

--- search/aux.h
+++ search/aux.h
@@ -9,10 +9,7 @@
 #include <sys/time.h>
 #include "kernels.h"
 
-#ifdef MAC
-    //#include <clapack.h>
-    //#include <cblas.h>
-#elif defined MKL
+#ifdef MKL
     #include <mkl.h>
     #include <mkl_service.h>
 #endif
@@ -20,8 +17,8 @@
 // BLAS & LAPACK function definitions
 extern "C" {
 
+/* BLAS defs */
 #define CBLAS_INDEX int
-
 enum CBLAS_ORDER {CblasRowMajor=101, CblasColMajor=102 };
 enum CBLAS_TRANSPOSE {CblasNoTrans=111, CblasTrans=112, CblasConjTrans=113,
 	AtlasConj=114};
@@ -29,6 +26,7 @@ enum CBLAS_UPLO  {CblasUpper=121, CblasLower=122};
 enum CBLAS_DIAG  {CblasNonUnit=131, CblasUnit=132};
 enum CBLAS_SIDE  {CblasLeft=141, CblasRight=142};
 
+/* BLAS routines */
 void cblas_dcopy(const int N, const double *X, const int incX,
                  double *Y, const int incY);
 CBLAS_INDEX cblas_idamax(const int N, const double *X, const int incX);
@@ -45,6 +43,25 @@ void cblas_dsyrk(const enum CBLAS_ORDER Order, const enum CBLAS_UPLO Uplo,
                  const enum CBLAS_TRANSPOSE Trans, const int N, const int K,
                  const double alpha, const double *A, const int lda,
                  const double beta, double *C, const int ldc);
+
+/* LAPACK defs */
+typedef int 		__CLPK_integer;
+typedef double 		__CLPK_doublereal;
+
+/* LAPACK routines   */    
+int dposv_(char *uplo, __CLPK_integer *n, __CLPK_integer *nrhs, __CLPK_doublereal 
+	*a, __CLPK_integer *lda, __CLPK_doublereal *b, __CLPK_integer *ldb, __CLPK_integer *info);
+int dgesvd_(char *jobu, char *jobvt, __CLPK_integer *m, __CLPK_integer *n, 
+	__CLPK_doublereal *a, __CLPK_integer *lda, __CLPK_doublereal *s, __CLPK_doublereal *u, __CLPK_integer *
+	ldu, __CLPK_doublereal *vt, __CLPK_integer *ldvt, __CLPK_doublereal *work, __CLPK_integer *lwork, 
+	__CLPK_integer *info);
+int dgeqrf_(__CLPK_integer *m, __CLPK_integer *n, __CLPK_doublereal *a, __CLPK_integer *
+	lda, __CLPK_doublereal *tau, __CLPK_doublereal *work, __CLPK_integer *lwork, __CLPK_integer *info);
+int dgerqf_(__CLPK_integer *m, __CLPK_integer *n, __CLPK_doublereal *a, __CLPK_integer *
+	lda, __CLPK_doublereal *tau, __CLPK_doublereal *work, __CLPK_integer *lwork, __CLPK_integer *info);
+int dormrq_(char *side, char *trans, __CLPK_integer *m, __CLPK_integer *n, 
+	__CLPK_integer *k, __CLPK_doublereal *a, __CLPK_integer *lda, __CLPK_doublereal *tau, __CLPK_doublereal *
+	c__, __CLPK_integer *ldc, __CLPK_doublereal *work, __CLPK_integer *lwork, __CLPK_integer *info);
 }
 
 // command line parsing functions
