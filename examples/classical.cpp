79eb00a87a0989036011b36abca208b5a7fc2818
Austin Benson <arbenson@gmail.com>
2014-10-09 11:47:02 -0700

    Clean up examples

--- examples/classical.cpp
+++ examples/classical.cpp
@@ -5,22 +5,21 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-    int m = 2000;
-    int k = 1200;
-    int n = 2000;
-    int numsteps = 1;
+  int m = 2000;
+  int k = 1200;
+  int n = 2000;
+  int numsteps = 1;
 
+  Matrix<double> A = RandomMatrix<double>(m, k);
+  Matrix<double> B = RandomMatrix<double>(k, n);
+  Matrix<double> C1(m, n), C2(m, n);
 
-    Matrix<double> A = RandomMatrix<double>(m, k);
-    Matrix<double> B = RandomMatrix<double>(k, n);
-    Matrix<double> C1(m, n), C2(m, n);
+  Time([&] { MatMul(A, B, C1); }, "Classical gemm");
+  Time([&] { classical222_8_24::FastMatmul(A, B, C2, numsteps); },
+       "Classical recursive (2, 2, 2)");
 
-    Time([&] { MatMul(A, B, C1); }, "Classical gemm");
-	Time([&] { classical222_8_24::FastMatmul(A, B, C2, numsteps); },
-		 "Classical recursive (2, 2, 2)");
+  // Test for correctness.
+  std::cout << "Maximum relative difference: " << MaxRelativeDiff(C1, C2) << std::endl;
 
-    // Test for correctness.
-    std::cout << "Maximum relative difference: " << MaxRelativeDiff(C1, C2) << std::endl;
-
-    return 0;
+  return 0;
 }
--- examples/fast333.cpp
+++ examples/fast333.cpp
@@ -5,22 +5,22 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-    int m = 9000;
-    int k = 9000;
-    int n = 9000;
-    int numsteps = 1;
+  int m = 9000;
+  int k = 9000;
+  int n = 9000;
+  int numsteps = 1;
 
-    Matrix<double> A = RandomMatrix<double>(m, k);
-    Matrix<double> B = RandomMatrix<double>(k, n);
-    Matrix<double> C1(m, n), C2(m, n);
-	
-    Time([&] { MatMul(A, B, C1); }, "Classical gemm");
-	Time([&] { grey333_23_152::FastMatmul(A, B, C2, numsteps); },
-		 "Fast (3, 3, 3)");
+  Matrix<double> A = RandomMatrix<double>(m, k);
+  Matrix<double> B = RandomMatrix<double>(k, n);
+  Matrix<double> C1(m, n), C2(m, n);
+    
+  Time([&] { MatMul(A, B, C1); }, "Classical gemm");
+  Time([&] { grey333_23_152::FastMatmul(A, B, C2, numsteps); },
+       "Fast (3, 3, 3)");
 
-    // Test for correctness.
-    std::cout << "Maximum relative difference: " << MaxRelativeDiff(C1, C2)
-			  << std::endl;
+  // Test for correctness.
+  std::cout << "Maximum relative difference: " << MaxRelativeDiff(C1, C2)
+            << std::endl;
 
-    return 0;
+  return 0;
 }
