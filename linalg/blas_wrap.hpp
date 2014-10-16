c09aaee340c044dfe97e1286c57933238377bb5d
Austin Benson <arbenson@gmail.com>
2014-10-16 10:06:32 -0700

    Make BLAS routines less fragile

--- linalg/blas_wrap.hpp
+++ linalg/blas_wrap.hpp
@@ -1,6 +1,8 @@
 #ifndef _BLAS_WRAP_HPP_
 #define _BLAS_WRAP_HPP_
 
+#include "common.hpp"
+
 #ifdef __INTEL_MKL__
 # include "mkl.h"
 # include "mkl_types.h"
@@ -9,26 +11,38 @@
 #include <functional>
 #include <stdexcept>
 
+#if defined(BLAS_POST)
+# define BLAS_NAME(name) name ## _
+#else
+# define BLAS_NAME(name) name
+#endif
+
 extern "C" {
-  void dgemm_(char *transa, char *transb, int *m, int *n, int *k,
-              double *alpha, double *a, int *lda, double *b, int *ldb,
-              double *beta, double *c, int *ldc);
-  void sgemm_(char *transa, char *transb, int *m, int *n, int *k,
-              float *alpha, float *a, int *lda, float *b, int *ldb,
-              float *beta, float *c, int *ldc);
+  void BLAS_NAME(dgemm) (char *transa, char *transb, int *m, int *n, int *k,
+                         double *alpha, double *a, int *lda, double *b,
+                         int *ldb, double *beta, double *c, int *ldc);
+  void BLAS_NAME(sgemm) (char *transa, char *transb, int *m, int *n, int *k,
+                         float *alpha, float *a, int *lda, float *b, int *ldb,
+                         float *beta, float *c, int *ldc);
 
-  void daxpy_(int *n, double *alpha, double *A, int *incx, double *C, int *incy);
-  void saxpy_(int *n, float *alpha, float *A, int *incx, float *C, int *incy);
+  void BLAS_NAME(daxpy) (int *n, double *alpha, double *A, int *incx, double *C,
+                         int *incy);
+  void BLAS_NAME(saxpy) (int *n, float *alpha, float *A, int *incx, float *C,
+                         int *incy);
 
-  void dtrsm_(char *side, char *uplo, char *transa, char *diag, int *m, int *n,
-              double *alpha, double *A, int *lda, double *B, int *ldb);
-  void strsm_(char *side, char *uplo, char *transa, char *diag, int *m, int *n,
-              float *alpha, float *A, int *lda, float *B, int *ldb);
+  void BLAS_NAME(dtrsm) (char *side, char *uplo, char *transa, char *diag,
+                         int *m, int *n, double *alpha, double *A, int *lda,
+                         double *B, int *ldb);
+  void BLAS_NAME(strsm) (char *side, char *uplo, char *transa, char *diag,
+                         int *m, int *n, float *alpha, float *A, int *lda,
+                         float *B, int *ldb);
 
-  void dtrmm_(char *side, char *uplo, char *transt, char *diag, int *m, int *n,
-              double *alpha, double *T, int *ldt, double *B, int *ldb);
-  void strmm_(char *side, char *uplo, char *transt, char *diag, int *m, int *n,
-              float *alpha, float *T, int *ldt, float *B, int *ldb);
+  void BLAS_NAME(dtrmm) (char *side, char *uplo, char *transt, char *diag,
+                         int *m, int *n, double *alpha, double *T, int *ldt,
+                         double *B, int *ldb);
+  void BLAS_NAME(strmm) (char *side, char *uplo, char *transt, char *diag,
+                         int *m, int *n, float *alpha, float *T, int *ldt,
+                         float *B, int *ldb);
 }
 
 
@@ -56,50 +70,50 @@ namespace blas {
 
   void Trmm(char side, char uplo, char transt, char diag, int m, int n,
             double alpha, double *T, int ldt, double *B, int ldb) {
-    dtrmm_(&side, &uplo, &transt, &diag, &m, &n, &alpha, T, &ldt, B, &ldb);
+    BLAS_NAME(dtrmm) (&side, &uplo, &transt, &diag, &m, &n, &alpha, T, &ldt, B, &ldb);
   }
 
 
   void Trmm(char side, char uplo, char transt, char diag, int m, int n,
             float alpha, float *T, int ldt, float *B, int ldb) {
-    strmm_(&side, &uplo, &transt, &diag, &m, &n, &alpha, T, &ldt, B, &ldb);
+    BLAS_NAME(strmm) (&side, &uplo, &transt, &diag, &m, &n, &alpha, T, &ldt, B, &ldb);
   }
 
 
   void Trsm(char side, char uplo, char transa, char diag, int m, int n,
             double alpha, double *A, int lda, double *B, int ldb) {
-    dtrsm_(&side, &uplo, &transa, &diag, &m, &n, &alpha, A, &lda, B, &ldb);
+    BLAS_NAME(dtrsm) (&side, &uplo, &transa, &diag, &m, &n, &alpha, A, &lda, B, &ldb);
   }
 
 
   void Trsm(char side, char uplo, char transa, char diag, int m, int n,
             float alpha, float *A, int lda, float *B, int ldb) {
-    strsm_(&side, &uplo, &transa, &diag, &m, &n, &alpha, A, &lda, B, &ldb);
+    BLAS_NAME(strsm) (&side, &uplo, &transa, &diag, &m, &n, &alpha, A, &lda, B, &ldb);
   }
 
   // Wrapper for dgemm called by templated gemm.
   void Gemm(char transa, char transb, int m, int n, int k, double *A, int lda,
             double *B, int ldb, double *C, int ldc, double alpha, double beta) {
-    dgemm_(&transa, &transb, &m, &n, &k, &alpha, A, &lda, B, &ldb, &beta,
-           C, &ldc);
+    BLAS_NAME(dgemm) (&transa, &transb, &m, &n, &k, &alpha, A, &lda, B, &ldb, &beta,
+                      C, &ldc);
   }
 
   // Wrapper for sgemm called by templated gemm.
   void Gemm(char transa, char transb, int m, int n, int k, float *A, int lda,
             float *B, int ldb, float *C, int ldc, float alpha, float beta) {
-    sgemm_(&transa, &transb, &m, &n, &k, &alpha, A, &lda, B, &ldb, &beta,
-           C, &ldc);
+    BLAS_NAME(sgemm) (&transa, &transb, &m, &n, &k, &alpha, A, &lda, B, &ldb, &beta,
+                      C, &ldc);
   }
 
   // C <-- alpha * A + C
   void Axpy(double *C, double *A, int n, double alpha, int incx, int incy) {
-    daxpy_(&n, &alpha, A, &incx, C, &incy);
+    BLAS_NAME(daxpy) (&n, &alpha, A, &incx, C, &incy);
   }
 
 
   // C <-- alpha * A + C
   void Axpy(float *C, float *A, int n, float alpha, int incx, int incy) {
-    saxpy_(&n, &alpha, A, &incx, C, &incy);
+    BLAS_NAME(saxpy) (&n, &alpha, A, &incx, C, &incy);
   }
 
 }  // end namespace blas
