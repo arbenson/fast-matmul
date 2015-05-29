Fix bug in q computation.

codegen/stability_vector.py
@@ -50,7 +50,7 @@ def main():
 
     W_ind = np.array([[float(val) for val in row] for row in coeffs[2]])
     W_ind[np.nonzero(W_ind)] = 1
-    q_vec = [gamma_vec[k] + np.max(np.dot(ab_vec, W_ind[k,:])) \
+    q_vec = [gamma_vec[k] + np.max(ab_vec * W_ind[k,:]) \
                  for k in range(W_ind.shape[0])]
 
     # Number of additions
@@ -63,10 +63,15 @@ def main():
 
     if full_stab_mat:
         # Print in the same style as D'Alberto presents in his 2014 paper.
-        print 'stability vector:'
+        print 'e vector:'
         for i in range(dims[0]):
             out_format = '\t' + ' '.join(['%4d' for _ in range(dims[2])])
             vals = e_vec[(i * dims[2]):(i * dims[2] + dims[2])]
+            print out_format % tuple(vals)
+        print 'q vector:'
+        for i in range(dims[0]):
+            out_format = '\t' + ' '.join(['%4d' for _ in range(dims[2])])
+            vals = q_vec[(i * dims[2]):(i * dims[2] + dims[2])]
             print out_format % tuple(vals)
 
 if __name__ == '__main__':